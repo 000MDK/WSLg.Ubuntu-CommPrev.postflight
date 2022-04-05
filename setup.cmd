9b10faefc2ad5f7ab4da278723f7587742f5293a
MDK <86747696+000MDK@users.noreply.github.com>
2022-04-05 20:30:53 +0200

    Update setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -1,8 +1,8 @@
 copy .\systemd-sysusers.service \\wsl.localhost\Ubuntu-CommPrev\tmp\systemd-sysusers.service
 copy .\bash.bashrc \\wsl.localhost\Ubuntu-CommPrev\tmp\bash.bashrc
-copy .\fonts.conf  copy \\wsl.localhost\Ubuntu-CommPrev\tmp\fonts.conf
-copy .\sudoers copy \\wsl.localhost\Ubuntu-CommPrev\tmp\sudoers
-copy .\setup.sh copy \\wsl.localhost\Ubuntu-CommPrev\tmp\setup.sh
+copy .\fonts.conf \\wsl.localhost\Ubuntu-CommPrev\tmp\fonts.conf
+copy .\sudoers \\wsl.localhost\Ubuntu-CommPrev\tmp\sudoers
+copy .\setup.sh \\wsl.localhost\Ubuntu-CommPrev\tmp\setup.sh
 ubuntupreview.exe run "sudo -s chmod +x /tmp/setup.sh"
 ubuntupreview.exe run "sudo -s /tmp/setup.sh"
 ubuntupreview.exe run "genie -i"
