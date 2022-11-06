dccce58a6a8d7a29884166c73ae05009d33336dc
MDK <86747696+000MDK@users.noreply.github.com>
2022-11-06 07:07:06 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,36 +12,7 @@ locale-gen en_US.UTF-8
 
 update-locale LANG=en_US.UTF-8
 
-wget -O /etc/apt/trusted.gpg.d/wsl-transdebian.gpg https://arkane-systems.github.io/wsl-transdebian/apt/wsl-transdebian.gpg
-
-chmod a+r /etc/apt/trusted.gpg.d/wsl-transdebian.gpg
-
-cat << EOF > /etc/apt/sources.list.d/wsl-transdebian.list
-deb https://arkane-systems.github.io/wsl-transdebian/apt/ jammy main
-deb-src https://arkane-systems.github.io/wsl-transdebian/apt/ jammy main
-EOF
-
-apt update
-
-apt install -y update-manager-core pulseaudio-dummy xwayland-dummy
-
-#apt install -y systemd-genie
-
-#systemctl disable apparmor.service
-
-#systemctl disable auditd.service
-
-#touch /etc/cloud/cloud-init.disabled
-
-#systemctl disable multipathd.service
-
-#systemctl disable NetworkManager-wait-online.service
-
-#systemctl disable systemd-modules-load.service
-
-#systemctl disable NetworkManager-wait-online.service
-
-#systemctl set-default multi-user.target
+apt install -y update-manager-core
 
 ssh-keygen -A
 
@@ -73,6 +44,3 @@ apt autoremove -y
 #rm -f /etc/fonts/fonts.conf
 #cp /tmp/fonts.conf /etc/fonts/fonts.conf
 #chmod 644 /etc/fonts/fonts.conf
-#rm -f /usr/lib/systemd/system/systemd-sysusers.service
-#cp /tmp/systemd-sysusers.service /usr/lib/systemd/system/systemd-sysusers.service
-#chmod 644 /usr/lib/systemd/system/systemd-sysusers.service
